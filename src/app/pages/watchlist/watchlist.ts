5d5434b67643172bf6c2119ce15576594cb00ebb
mathildahere <mathilda.here@gmail.com>
2026-01-06 09:52:05 +0700

    feat: implement sidebar watchlist, settings, and logout features

---
+++ src/app/pages/watchlist/watchlist.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-watchlist',
+  templateUrl: './watchlist.html',
+  styleUrl: './watchlist.scss',
+  standalone: true,
+})
+export class Watchlist {}
